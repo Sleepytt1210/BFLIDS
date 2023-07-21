577b31af3b96b667753eaadd9fa570767418d7c9
Dylon Wong <sleepytt1210@gmail.com>
2023-07-21 21:27:20 +0800

    Tensorflow client with gateway and ipfs process

--- application/fed-learn/client.py
+++ application/fed-learn/client.py
@@ -1,102 +1,133 @@
-from typing import Dict, List, Tuple
-from collections import OrderedDict
-
-import torch
-import numpy as np
 import flwr as fl
+import tensorflow as tf
 import models.net as net
-from flwr.common import NDArrays, Scalar, Metrics
+from data.loader import load_data
+from utils.config import NUM_CLIENTS, CID, S_ADDR
+from subprocess import Popen
+import os
+import os.path as path
+import utils.config as cfg
+import ipfsApi
+import time
 
-DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
-print(
-    f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
-)
+class BFLClient(fl.client.NumPyClient):
 
-def train(net, trainloader, epochs: int, verbose=False):
-    """Train the network on the training set."""
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters())
-    net.train()
-    for epoch in range(epochs):
-        correct, total, epoch_loss = 0, 0, 0.0
-        for images, labels in trainloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-            optimizer.zero_grad()
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            # Metrics
-            epoch_loss += loss
-            total += labels.size(0)
-            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-        epoch_loss /= len(trainloader.dataset)
-        epoch_acc = correct / total
-        if verbose:
-            print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
-
-
-def test(net, testloader):
-    """Evaluate the network on the entire test set."""
-    criterion = torch.nn.CrossEntropyLoss()
-    correct, total, loss = 0, 0, 0.0
-    net.eval()
-    with torch.no_grad():
-        for images, labels in testloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    loss /= len(testloader.dataset)
-    accuracy = correct / total
-    return loss, accuracy
-
-def get_parameters(net) -> List[np.ndarray]:
-    return [val.cpu().numpy() for _, val in net.state_dict().items()]
-
-
-def set_parameters(net, parameters: List[np.ndarray]):
-    params_dict = zip(net.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-    net.load_state_dict(state_dict, strict=True)
-
-class Client(fl.client.NumPyClient):
-    def __init__(
-        self,
-        model: net.Net,
-        trainloader: torch.utils.data.DataLoader,
-        testloader: torch.utils.data.DataLoader,
-        num_examples: Dict,
-    ) -> None:
+    def __init__(self, cid: int, model: tf.keras.Model, x_train, y_train, x_test, y_test) -> None:
         self.model = model
-        self.trainloader = trainloader
-        self.testloader = testloader
-        self.num_examples = num_examples
+        self.cid = cid
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        
+        self.gateway_ps: Popen = None
+        self.ipfs_daemon: Popen = None
+        self.ipfs_connection: Popen = None
+        self.ipfs_id = None
+        self.__setup()
 
-    def get_parameters(self, config) -> List[np.ndarray]:
-        # Return model parameters as a list of NumPy ndarrays
-        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+    def get_parameters(self, config):
+        return self.model.get_weights()
 
-    def set_parameters(self, parameters: List[np.ndarray]) -> None:
-        # Set model parameters from a list of NumPy ndarrays
-        params_dict = zip(self.model.state_dict().keys(), parameters)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-        self.model.load_state_dict(state_dict, strict=True)
+    def fit(self, parameters, config):
+        self.model.set_weights(parameters)
+        with tf.device('/device:gpu:0'):
+            self.model.fit(self.x_train, self.y_train, epochs=1, batch_size=32)
+        return self.model.get_weights(), len(self.x_train), {}
 
-    def fit(
-        self, parameters: List[np.ndarray], config: Dict[str, str]
-    ) -> Tuple[List[np.ndarray], int, Dict]:
-        # Set model parameters, train model, return updated model parameters
-        self.set_parameters(parameters)
-        train(self.model, self.trainloader, epochs=1, device=DEVICE)
-        return self.get_parameters(config={}), self.num_examples["trainset"], {}
+    def evaluate(self, parameters, config):
+        self.model.set_weights(parameters)
+        loss, accuracy = self.model.evaluate(self.x_test, self.y_test)
+        return loss, len(self.x_test), {"accuracy": float(accuracy)}
+    
+    def terminate(self):
+        if self.gateway_ps:
+            self.gateway_ps.terminate()
+            self.gateway_ps = None
+        if self.ipfs_daemon:
+            self.ipfs_daemon.terminate()
+            self.ipfs_daemon = None
+        if self.ipfs_connection:
+            self.ipfs_connection.terminate()
+            self.ipfs_connection = None
 
-    def evaluate(
-        self, parameters: List[np.ndarray], config: Dict[str, str]
-    ) -> Tuple[float, int, Dict]:
-        # Set model parameters, evaluate model on local test dataset, return result
-        self.set_parameters(parameters)
-        loss, accuracy = test(self.model, self.testloader, device=DEVICE)
-        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy)}
+    def __setup(self):
+        self.terminate()
+
+        env_vars = self.__setup_env()
+
+        print(f"[CLIENT {self.cid}]: Starting blockchain gateway")
+        self.gateway_ps = Popen(
+            ['npm', 'run', 'start'], 
+            env=env_vars,
+            cwd=os.path.join(os.curdir, '..', 'agent')
+        )
+
+        time.sleep(5)
+
+        print(f"[CLIENT {self.cid}]: Starting IPFS daemon")
+        self.ipfs_daemon = Popen(
+            ['ipfs.sh', 'setup'],
+            env=env_vars,
+        )
+
+        api = ipfsApi.Client(env_vars['IPFS_HOST'], env_vars["IPFS_API_PORT"])
+        self.ipfs_id = api.id()
+        
+        s_target = env_vars["IPFS_SWARM_TARGET"]
+        if self.ipfs_id['Addresses'][0] != s_target:
+            print(f"[CLIENT {self.cid}]: Connecting to {s_target}")
+            api.swarm_connect(env_vars["IPFS_SWARM_TARGET"])
+
+    def __setup_env(self):
+        
+        cid = str(self.client.cid)
+        client_name = 'Org' + cid 
+        peer_domain = client_name.lower() + '.example.com'
+        env_vars = dict()
+        env_vars["ORG"] = os.environ.get("ORG", client_name)
+        env_vars["MSP_ID"] = os.environ.get("MSP_ID", client_name + "MSP")
+        crypto_path = os.path.join(os.curdir, '..', '..', 'network', 'organizations', 'peerOrganizations', peer_domain)
+        env_vars["KEY_DIRECTORY_PATH"] = os.environ.get("KEY_DIRECTORY_PATH", os.path.join(crypto_path, 'users', f'User1@{peer_domain}', 'msp', 'keystore')) 
+        env_vars["CERT_PATH"] = os.environ.get("CERT_PATH", os.path.join(crypto_path, 'users', f'User1@{peer_domain}', 'msp', 'signcerts', 'cert.pem'))
+        env_vars["TLS_CERT_PATH"] = os.environ.get("TLS_CERT_PATH", os.path.join(crypto_path, 'peers', f'peer0.{peer_domain}', 'tls', 'ca.crt'))
+        env_vars["PEER_HOST_ALIAS"] = peer_domain
+        env_vars["HOST"] = os.environ.get("C_HOST", "0.0.0.0")
+        env_vars["PORT"] = os.environ.get("C_PORT")
+        env_vars["EXPRESS_PORT"] = cfg.EXPRESS_PORT
+
+        env_vars["IPFS_PATH"] = os.environ.get("IPFS_PATH", os.path.join(os.curdir, '..', '..', 'ipfs-conf', peer_domain))
+        offset = 1000 * (int(cid) - 1)
+
+        env_vars["IPFS_HOST"] = os.environ.get("IPFS_HOST", "0.0.0.0")
+        env_vars["IPFS_GATEWAY_PORT"] = os.environ.get("IPFS_GATEWAY_PORT", 9898 + offset)
+        env_vars["IPFS_API_PORT"] = os.environ.get("IPFS_API_PORT", 5001 + offset)
+        env_vars["IPFS_SWARM_PORT"] = os.environ.get("IPFS_SWARM_PORT", 4001 + (int(cid) - 1))
+        env_vars["IPFS_SWARM_TARGET"] = cfg.IPFS_SWARM_TARGET
+
+        # Populate other env vars
+        for k, v in os.environ.items:
+            if env_vars.get(k) == None:
+                env_vars = v
+
+        return env_vars
+
+DATA_ROOT = path.abspath("data/datasets/")
+
+def main() -> None:
+    """Load data, start CifarClient."""
+
+    # Load model and data
+    print("Number of clients:", NUM_CLIENTS)
+
+    print("Loading model and data for client", CID)
+    x_train, x_test, y_train, y_test = load_data(DATA_ROOT, NUM_CLIENTS, CID)
+    model = net.get_model()
+
+    # Start client
+    print(f"Client connecting to server {S_ADDR}")
+    client = BFLClient(CID, model, x_train, y_train, x_test, y_test)
+    fl.client.start_numpy_client(server_address=S_ADDR, client=client)
+
+if __name__ == "__main__":
+    main()
