70a1f77f84bd9a9337c2c3b761693762f7b50700
Dylon Wong <sleepytt1210@gmail.com>
2023-07-21 21:31:20 +0800

    Tensorflow models

--- application/fed-learn/models/net.py
+++ application/fed-learn/models/net.py
@@ -1,23 +1,23 @@
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import Tensor
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import LSTM, Bidirectional, BatchNormalization, Conv1D, MaxPooling1D
 
-class Net(nn.Module):
+def get_model():
+    model = Sequential()
+    model.add(Conv1D(64, kernel_size=64, padding='same', activation='relu', input_shape=(196, 1)))
+    model.add(MaxPooling1D(pool_size=(10)))
+    model.add(BatchNormalization())
+    model.add(Bidirectional(LSTM(64, return_sequences=False)))
+    model.add(Dropout(0.6))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-    def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+def get_simple_model():
+    model = Sequential()
+    model.add(Conv1D(64, kernel_size=64, padding='same', activation='relu', input_shape=(196, 1)))
+    model.add(Dropout(0.7))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam' , metrics=['binary_accuracy', 'false_negatives', 'false_positives'])
+    return model
