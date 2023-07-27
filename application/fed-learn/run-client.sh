af6f0e919dbd4038e31a09cc24bae9a33f160e64
Dylon Wong <sleepytt1210@gmail.com>
2023-07-27 19:19:04 +0800

    export C_PORT env

--- application/fed-learn/run-client.sh
+++ application/fed-learn/run-client.sh
@@ -1,4 +1,20 @@
 export CLIENT_ID=$1
 
 echo "Starting Client $CLIENT_ID"
+export C_HOST='0.0.0.0'
+
+case "$CLIENTID" in
+    1 ) 
+        PORT='7051'
+        ;;
+    2 )
+        PORT='9051'
+        ;;
+    3 )
+        PORT='11051'
+        ;;
+esac
+
+export C_PORT=$PORT
+
 python client.py
