c039461c476bd7ffc51aa75b483bc4b0073ca6bb
Dylon Wong <sleepytt1210@gmail.com>
2023-08-04 16:16:21 +0800

    Metrics include sensitivity and speficiity

--- application/fed-learn/models/net.py
+++ application/fed-learn/models/net.py
@@ -1,6 +1,17 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers import LSTM, Bidirectional, BatchNormalization, Conv1D, MaxPooling1D
+import keras.backend as K
+
+def sensitivity(y_true, y_pred): 
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+def specificity(y_true, y_pred): 
+    true_positives = K.sum(K.round(K.clip(1 - y_true * 1 - y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(1 - y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
 
 def get_model():
     model = Sequential()
@@ -11,7 +22,7 @@ def get_model():
     model.add(Dropout(0.6))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', specificity, sensitivity])
     return model
 
 def get_simple_model():
@@ -19,5 +30,5 @@ def get_simple_model():
     model.add(Conv1D(64, kernel_size=64, padding='same', activation='relu', input_shape=(196, 1)))
     model.add(Dropout(0.7))
     model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam' , metrics=['binary_accuracy', 'false_negatives', 'false_positives'])
+    model.compile(loss='binary_crossentropy', optimizer='adam' , metrics=['accuracy', specificity, sensitivity])
     return model
